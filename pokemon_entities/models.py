8e51ed79b0b2720aab793f071c1199a53b85339d
ilyashirko <ilyashirko@gmail.com>
2022-04-08 14:31:34 +0300

    add specs to PokemonEntity model

--- pokemon_entities/models.py
+++ pokemon_entities/models.py
@@ -1,5 +1,4 @@
 from django.db import models  # noqa F401
-from datetime import datetime
 
 
 class Pokemon(models.Model):
@@ -11,11 +10,39 @@ class Pokemon(models.Model):
 
 
 class PokemonEntity(models.Model):
-    pokemon = models.ForeignKey(to=Pokemon, on_delete=models.CASCADE)
-    latitude = models.FloatField(max_length=15)
-    longitude = models.FloatField(max_length=15)
+    pokemon = models.ForeignKey(
+        verbose_name='Pokemon',
+        to=Pokemon,
+        on_delete=models.CASCADE
+        )
+
+    latitude = models.FloatField(max_length=15, blank=False)
+    longitude = models.FloatField(max_length=15, blank=False)
+    
     appeared_at = models.DateTimeField(default=None, blank=False)
     disappeared_at = models.DateTimeField(default=None, blank=False)
+
+    level = models.PositiveSmallIntegerField(
+        default=None,
+        blank=False
+    )
+    health = models.PositiveSmallIntegerField(
+        default=None,
+        blank=False
+    )
+    strength = models.PositiveSmallIntegerField(
+        default=None,
+        blank=False
+    )
+    defence = models.PositiveSmallIntegerField(
+        default=None,
+        blank=False
+    )
+    stamina = models.PositiveSmallIntegerField(
+        default=None,
+        blank=False
+    )
+
 
     def __str__(self):
         return f'{self.latitude}, {self.longitude}'
