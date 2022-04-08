3ff77b6910708b27e859c5d145ef0dbae6da8db4
ilyashirko <ilyashirko@gmail.com>
2022-04-08 13:52:42 +0300

    Add relation to db models

--- pokemon_entities/models.py
+++ pokemon_entities/models.py
@@ -12,6 +12,7 @@ class Pokemon(models.Model):
 class PokemonEntity(models.Model):
     latitude = models.FloatField(max_length=15)
     longitude = models.FloatField(max_length=15)
+    pokemon = models.ForeignKey(to=Pokemon, on_delete=models.CASCADE)
 
     def __str__(self):
         return f'{self.latitude}, {self.longitude}'
